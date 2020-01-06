f09bdeb4f36f16dad2d03f178f986f186dc1cf49
Laura Miller <lmiller.spence@gmail.com>
2020-01-06 14:45:26 +0000

    Created React app for todo list

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+    render() {
+      return (
+        <p>Here's another thing</p>
+      );
+    }
+  }
+  
+  export default Header;
