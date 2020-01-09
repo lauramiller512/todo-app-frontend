d280ef3eea5d359d53e0d897f4ca712ab3bc03ec
Laura Miller <lmiller.spence@gmail.com>
2020-01-09 09:21:00 +0000

    Attempting to make the plus image inline and smaller

--- src/SubmitButton.js
+++ src/SubmitButton.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import logo from './images/plus.png';
 
 class SubmitButton extends React.Component {
 
     render() {
       return (
           <div className="submit">
-            <span onclick="newElement()" class="addBtn">Submit</span>
+            <span onclick="newElement()" class="addBtn"><img src={logo} className="plus"></img></span>
           </div>
       );
     }
