21f944175287c470e93b2127b6fda856fb90e91d
Laura Miller <lmiller.spence@gmail.com>
2020-01-09 18:35:34 +0000

    Removed more obsolete CSS code

--- src/Footer.js
+++ src/Footer.js
@@ -4,10 +4,8 @@ class Footer extends React.Component {
 
     render() {
         return (
-            <div className="row">
-                <div className="footer col-12">
-                     <p>Footer</p>
-                </div>
+            <div class="footer">
+                <p>Footer</p>
             </div>
         );
     }
