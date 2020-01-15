6aaa5f160dce777e65ea3e32214232baeb1b747c
Laura Miller <lmiller.spence@gmail.com>
2020-01-15 17:09:34 +0000

    Started adding bootstrap to addtask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,18 +4,25 @@ class AddTask extends React.Component {
 
     render() {
         return (
-            <div className="row">
-                <div className="col-12">
-                    <div className="input-group mb-3">
-                        <input
-                            type="text"
-                            className="form-control"
-                            placeholder="Task to add...."
-                        />
-                        <div className="input-group-append">
-                            <button className="btn btn-outline-primary" type="button">
-                                Add
+            <div className="container">
+                <div className="row">
+                    <div className="col-5 offset-4 addtask">
+                        <h3>Add your task to the list here: </h3>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-5 offset-4">
+                        <div className="input-group mb-3">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Task to add...."
+                            />
+                            <div className="input-group-append">
+                                <button className="btn btn-outline-primary" type="button">
+                                    Add
               </button>
+                            </div>
                         </div>
                     </div>
                 </div>
