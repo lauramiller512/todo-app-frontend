6f910e4f1da44f1ecaddfab30194d8f2ee1662f8
Laura Miller <lmiller.spence@gmail.com>
2020-01-16 18:01:16 +0000

    Trying to make doneTasks array display in CompletedTasks

--- src/components/CompletedTasks.js
+++ src/components/CompletedTasks.js
@@ -4,18 +4,30 @@ class CompletedTasks extends React.Component {
 
   render() {
     return (
-      <div className="container">
+      /* <div className="container">
         <div className="row">
           <div className="col-12 col-md-8">
             <h3 className="completedTitle">Tasks Completed: </h3>
+            <div className="col-6 col-md-6">
               <ul className="tasksCompleted">
                 <li>Watch a film</li>
                 <li>Call the bank</li>
                 <li>Renew driver's license</li>
               </ul>
+              </div>
             </div>
           </div>
+    </div> */
+
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-8">
+            <ol>
+              {doneTasks.map(doneTasks => <li>{doneTasks}</li>)}
+            </ol>
+          </div>
         </div>
+      </div >
     );
   }
 }
