8ee83ef0f19a553d0fb599e4c97697036cb1b196
Laura Miller <lmiller.spence@gmail.com>
2020-01-16 16:45:28 +0000

    Yet more css - removing bullet points from completed tasks list

--- src/components/CompletedTasks.js
+++ src/components/CompletedTasks.js
@@ -4,13 +4,17 @@ class CompletedTasks extends React.Component {
 
   render() {
     return (
-        <div className="tasksCompleted">
-            <h3>Tasks Completed: </h3>
-            <ul>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-8">
+            <h3 className="completedTitle">Tasks Completed: </h3>
+              <ul className="tasksCompleted">
                 <li>Watch a film</li>
                 <li>Call the bank</li>
                 <li>Renew driver's license</li>
-            </ul>
+              </ul>
+            </div>
+          </div>
         </div>
     );
   }
