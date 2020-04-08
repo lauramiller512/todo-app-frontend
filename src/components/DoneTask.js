4d6d206fc6e89dc0ecf4d869ee18f5388ee83f2e
Laura Miller <lmiller.spence@gmail.com>
2020-04-08 14:08:46 +0100

    Removed hardcoded array

--- src/components/DoneTask.js
+++ src/components/DoneTask.js
@@ -8,8 +8,7 @@ class DoneTasks extends React.Component {
         <div className="row">
           <div className="col-6 offset-3">
             <h3 className="completedTitle">Tasks Completed: </h3>
-            <div className="col-12">
-              <ul className="tasksCompleted">
+         
                 {/* Add keys to each list item, set up like:
                 const people = ['Rowe', 'Prevost', 'Gare'];
 
@@ -17,11 +16,8 @@ const peopleLis = people.map((person, i) =>
   // expression goes here:
   <li key={'person_' + i}>{person}</li>;
 ); */}
-                <li>Watch a film</li>
-                <li>Call the bank</li>
-                <li>Renew driver's license</li>
-              </ul>
-              </div>
+              
+           
             </div>
           </div>
           </div>
