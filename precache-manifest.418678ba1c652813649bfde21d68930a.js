32ab4dc73f9207fdf7843a66afc6076b90c0b9f9
Laura Miller <lmiller.spence@gmail.com>
2020-04-17 11:22:47 +0100

    Updates

---
+++ precache-manifest.418678ba1c652813649bfde21d68930a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b81f1b8e61b8447d823673b39b8e9af",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "bdb19b80f2db982fae2c",
+    "url": "/react-gh-pages/static/css/main.ac192936.chunk.css"
+  },
+  {
+    "revision": "912ca779febf98932883",
+    "url": "/react-gh-pages/static/js/2.526a3fc8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-gh-pages/static/js/2.526a3fc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bdb19b80f2db982fae2c",
+    "url": "/react-gh-pages/static/js/main.0b265cb5.chunk.js"
+  },
+  {
+    "revision": "7735f525516992b716ea",
+    "url": "/react-gh-pages/static/js/runtime-main.59cf04e2.js"
+  },
+  {
+    "revision": "c1be93bb15c175b027ec262c93e0cbd7",
+    "url": "/react-gh-pages/static/media/blueback.c1be93bb.jpg"
+  }
+]);
